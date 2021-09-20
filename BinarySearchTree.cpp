af5928fab20ffd24a1b8466b93e89726af23e5ff
LucasLazzaris <lucascercal.l@gmail.com>
2021-09-20 20:00:58 -0300

    Implement Search, min, max, successor and insert on a binary search tree

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -33,12 +33,14 @@ public:
     return parent;
   }
 
-  void setLeft(Node<T>* const leftNode){
+  void setLeft(Node<T>* leftNode){
     left = leftNode;
+    leftNode->setParent(this);
   }
 
-  void setRight(Node<T>* const rightNode){
+  void setRight(Node<T>* rightNode){
     right = rightNode;
+    rightNode->setParent(this);
   }
 
   void setParent(Node<T>* const parentNode){
@@ -83,9 +85,88 @@ public:
   void inorder_tree_walk(Node<T>* const node) const {
     if(node != nullptr){
       inorder_tree_walk(node->getLeft());
-      std::cout << node->getValue();
+      std::cout << node->getValue() << std::endl;
       inorder_tree_walk(node->getRight());
     }
+  }
+
+  Node<T>* searchNode(Node<T>* const node, const T& value) {
+    if (node == nullptr ){
+      return node;
+    } 
+
+    auto nodeValue = node->getValue();
+
+    if(value == nodeValue) {
+      return node;
+    } else if (value < nodeValue) {
+      return searchNode(node->getLeft(), value);
+    } else {
+      return searchNode(node->getRight(), value);
+    }
+  }
+
+  Node<T>* iterativeSearchNode(Node<T>* const startNode, const T& value){
+    Node<T>* node = startNode;
+    while (node != nullptr && node->getValue() != value) {
+      if (value < node->getValue()) {
+        node = node->getLeft();
+      } else {
+        node = node->getRight();
+      }
+    }
+    return node;
+  }
+
+  Node<T>* minimum(Node<T>* const startNode){
+    Node<T>* current = startNode;
+    while(current->getLeft() != nullptr){
+      current = current->getLeft();
+    }
+    return current;
+  }
+
+  Node<T>* maximum(Node<T>* const startNode){
+    Node<T>* current = startNode;
+    while(current->getRight() != nullptr){
+      current = current->getRight();
+    }
+    return current;
+  }
+
+  Node<T>* successor(Node<T>* const startNode) {
+    if(startNode->getRight() != nullptr) {
+      return minimum(startNode->getRight());
+    }
+    auto previous = startNode;
+    auto current = startNode->getParent();
+    while(current != nullptr && previous == current->getRight()) {
+      previous = current;
+      current = current->getParent();
+    }
+    return current;
+  }
+
+  void insert(Node<T>* const newNode){
+    Node<T>* current = nullptr;
+    Node<T>* next = root;
+    while(next != nullptr){
+      current = next;
+      if(newNode->getValue() < next->getValue()) {
+        next = next->getLeft();
+      } else {
+        next = next->getRight();
+      }
+    }
+    newNode->setParent(current);
+    if(current == nullptr){
+      root = newNode;
+    } else if (newNode->getValue() < current->getValue()){
+      current->setLeft(newNode);
+    } else {
+      current->setRight(newNode);
+    }
+    size++;
   }
 
   Node<T>* getRoot() {
@@ -100,3 +181,21 @@ private:
   Node<T>* root{nullptr};
   unsigned int size{0};
 };
+
+// int main(void) {
+//   Node<int>* node1 = new(Node<int>);
+//   Node<int>* node2 = new(Node<int>);
+//   Node<int>* node3 = new(Node<int>);
+//   Node<int>* node4 = new(Node<int>);
+//   node1->setValue(10);
+//   node2->setValue(9);
+//   node3->setValue(11);
+//   node4->setValue(7);
+//   BinarySearchTree<int> tree{node1}; 
+//   node1->setLeft(node2);
+//   node1->setRight(node3);
+//   node2->setLeft(node4);
+
+//   tree.inorder_tree_walk(tree.getRoot());
+//   return 0;
+// }
--- tests/testBinarySearchTree.cpp
+++ tests/testBinarySearchTree.cpp
@@ -56,13 +56,166 @@ TEST_CASE("Test Binary Search Tree Constructors", "[BinarySearchTree]"){
         REQUIRE(tree.getSize() == 1);
         CHECK(tree.getRoot() == node);
     }
+}
 
-    // SECTION("Copy constructor does not copy pointer"){
-    //     BinarySearchTree<int> a1(4);
-    //     BinarySearchTree<int> a2(a1);
-    //     a1[1] = 2;
-    //     a2[1] = 3;
-    //     CHECK(a1[1] == 2);
-    //     REQUIRE(a2[1] == 3);
-    // }
+TEST_CASE("searchNode", "[BinarySearchTree]"){
+    Node<int>* node1 = new(Node<int>);
+    Node<int>* node2 = new(Node<int>);
+    Node<int>* node3 = new(Node<int>);
+    Node<int>* node4 = new(Node<int>);
+    node1->setValue(10);
+    node2->setValue(9);
+    node3->setValue(11);
+    node4->setValue(7);
+    node1->setLeft(node2);
+    node1->setRight(node3);
+    node2->setLeft(node4);
+    BinarySearchTree<int> tree{node1}; 
+
+    SECTION("Minimum") {
+        auto nodeFound = tree.searchNode(tree.getRoot(), 7);
+        REQUIRE(nodeFound == node4);
+    }
+    SECTION("Maximum") {
+        auto nodeFound = tree.searchNode(tree.getRoot(), 11);
+        REQUIRE(nodeFound == node3);
+    }
+    SECTION("Middle") {
+        auto nodeFound = tree.searchNode(tree.getRoot(), 9);
+        REQUIRE(nodeFound == node2);
+    }
+    SECTION("Not Found") {
+        auto nodeFound = tree.searchNode(tree.getRoot(), 19);
+        REQUIRE(nodeFound == nullptr);
+    }
+}
+
+TEST_CASE("iterativeSearchNode", "[BinarySearchTree]"){
+    Node<int>* node1 = new(Node<int>);
+    Node<int>* node2 = new(Node<int>);
+    Node<int>* node3 = new(Node<int>);
+    Node<int>* node4 = new(Node<int>);
+    node1->setValue(10);
+    node2->setValue(9);
+    node3->setValue(11);
+    node4->setValue(7);
+    node1->setLeft(node2);
+    node1->setRight(node3);
+    node2->setLeft(node4);
+    BinarySearchTree<int> tree{node1}; 
+
+    SECTION("Minimum") {
+        auto nodeFound = tree.iterativeSearchNode(tree.getRoot(), 7);
+        REQUIRE(nodeFound == node4);
+    }
+    SECTION("Maximum") {
+        auto nodeFound = tree.iterativeSearchNode(tree.getRoot(), 11);
+        REQUIRE(nodeFound == node3);
+    }
+    SECTION("Middle") {
+        auto nodeFound = tree.iterativeSearchNode(tree.getRoot(), 9);
+        REQUIRE(nodeFound == node2);
+    }
+    SECTION("Not Found") {
+        auto nodeFound = tree.iterativeSearchNode(tree.getRoot(), 19);
+        CHECK(nodeFound == nullptr);
+        CHECK(tree.getRoot() == node1);
+        CHECK(node1->getLeft() == node2);
+        CHECK(node1->getRight() == node3);
+        REQUIRE(node2->getLeft() == node4);
+    }
+}
+
+
+TEST_CASE("Minimum and Maximum", "[BinarySearchTree]") {
+    Node<int>* node1 = new(Node<int>);
+    Node<int>* node2 = new(Node<int>);
+    Node<int>* node3 = new(Node<int>);
+    Node<int>* node4 = new(Node<int>);
+    node1->setValue(10);
+    node2->setValue(9);
+    node3->setValue(11);
+    node4->setValue(7);
+    node1->setLeft(node2);
+    node1->setRight(node3);
+    node2->setLeft(node4);
+    BinarySearchTree<int> tree{node1}; 
+
+    SECTION("Minimum") {
+        auto nodeFound = tree.minimum(tree.getRoot());
+        REQUIRE(nodeFound == node4);
+    }
+    SECTION("Maximum") {
+        auto nodeFound = tree.maximum(tree.getRoot());
+        REQUIRE(nodeFound == node3);
+    }
+}
+
+TEST_CASE("Successor", "[BinarySearchTree]") {
+    Node<int>* node1 = new(Node<int>);
+    Node<int>* node2 = new(Node<int>);
+    Node<int>* node3 = new(Node<int>);
+    Node<int>* node4 = new(Node<int>);
+    Node<int>* node5 = new(Node<int>);
+    Node<int>* node6 = new(Node<int>);
+    Node<int>* node7 = new(Node<int>);
+    Node<int>* node8 = new(Node<int>);
+    Node<int>* node9 = new(Node<int>);
+    Node<int>* node10 = new(Node<int>);
+    Node<int>* node11 = new(Node<int>);
+    node1->setValue(15);
+    node2->setValue(6);
+    node3->setValue(18);
+    node4->setValue(3);
+    node5->setValue(7);
+    node6->setValue(17);
+    node7->setValue(20);
+    node8->setValue(2);
+    node9->setValue(4);
+    node10->setValue(13);
+    node11->setValue(9);
+
+    node1->setLeft(node2);
+    node1->setRight(node3);
+    node2->setLeft(node4);
+    node2->setRight(node5);
+    node3->setLeft(node6);
+    node3->setRight(node7);
+    node4->setLeft(node8);
+    node4->setRight(node9);
+    node5->setRight(node10);
+    node10->setLeft(node11);
+
+    BinarySearchTree<int> tree{node1}; 
+
+    SECTION("Minimum") {
+        auto nodeFound = tree.successor(node8);
+        REQUIRE(nodeFound == node4);
+    }
+    SECTION("Maximum") {
+        auto nodeFound = tree.successor(node7);
+        REQUIRE(nodeFound == nullptr);
+    }
+    SECTION("Has Right sub tree") {
+        auto nodeFound = tree.successor(node5);
+        REQUIRE(nodeFound == node11);
+    }
+    SECTION("Doesn't have Right sub tree") {
+        auto nodeFound = tree.successor(node10);
+        REQUIRE(nodeFound == node1);
+    }
+}
+
+
+TEST_CASE("Insert", "[BinarySearchTree]") {
+    SECTION("Empty tree") {
+        Node<int>* node1 = new(Node<int>);
+        node1->setValue(15);
+        BinarySearchTree<int> tree; 
+        tree.insert(node1);
+        CHECK(tree.getRoot() == node1);
+        CHECK(node1->getLeft() == nullptr);
+        CHECK(node1->getRight() == nullptr);
+        REQUIRE(node1->getParent() == nullptr);
+    }
 }
